060e7e2a70eb9dce1ddfcad0ffbfcc3323cf5aaa
Attila Fejer <attila@fejer.io>
2023-09-05 16:46:34 +0200

    chore: added mockito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
     testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.mockito:mockito-core:5.5.0")
 }
 
 java {
