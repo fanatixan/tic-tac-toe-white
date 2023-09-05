69ebdf9a89d0b7f6baa37bd564d61fb8ed2f8159
Attila Fejer <attila@fejer.io>
2023-09-05 16:02:08 +0200

    feat(uat-2): green

--- app/src/main/java/io/fejer/tictactoe/Game.java
+++ app/src/main/java/io/fejer/tictactoe/Game.java
@@ -11,7 +11,7 @@ public class Game {
     public String print() {
         String state = "Player X moves";
 
-        if (board.get(6) == 'X') {
+        if (board.get(0) == board.get(3) && board.get(0) == board.get(6) && board.get(0) == 'X') {
             state = "Player X won";
         }
 
