e083cac1e609dd84d80cb82ac6eb56c38982b892
Attila Fejer <attila@fejer.io>
2023-09-06 09:51:58 +0200

    feat(bot): green

--- app/src/main/java/io/fejer/tictactoe/Bot.java
+++ app/src/main/java/io/fejer/tictactoe/Bot.java
@@ -4,14 +4,19 @@ import java.util.function.IntSupplier;
 
 public class Bot {
 
+    private Board board;
     private IntSupplier random;
 
     public Bot(Board board, IntSupplier random) {
-
+        this.board = board;
         this.random = random;
     }
 
     public int nextStep() {
-        return random.getAsInt();
+        int step = random.getAsInt();
+        if (board.get(step) != Board.EMPTY) {
+            step = random.getAsInt();
+        }
+        return step;
     }
 }
